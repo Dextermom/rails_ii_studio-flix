a64242e38314a62e72ba71e9afdc1991952a70c9
Julie Rybolt <julierybolt@Julies-MacBook-Air.local>
2018-02-27 23:32:49 -0600

    Exercise 3 User Accounts: UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+get 'signup' => 'users#new'
   resources :users
   root "movies#index"
   resources :movies do
     resources :reviews
   end
+
 end
---
+++ spec/features/list_users_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "Viewing the list of users" do
+
+  it "shows the users" do
+    user1 = User.create!(user_attributes(name: "Larry", username: "lareman", email: "larry@example.com"))
+    user2 = User.create!(user_attributes(name: "Moe",   username: "Moeman", email: "moe@example.com"))
+    user3 = User.create!(user_attributes(name: "Curly", username: "Curlman", email: "curly@example.com"))
+
+    visit users_url
+
+    expect(page).to have_link(user1.name)
+    expect(page).to have_link(user2.name)
+    expect(page).to have_link(user3.name)
+  end
+
+end
