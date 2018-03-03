09143173735992f03db1f592c9b2b3b75284d372
Julie Rybolt <julierybolt@Julies-MacBook-Air.local>
2018-03-02 23:57:25 -0600

    Exercise 12 Scopes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -19,18 +19,13 @@ class Movie < ApplicationRecord
   has_many :fans, through: :favorites, source: :user
   has_many :characterizations, dependent: :destroy
   has_many :genres, through: :characterizations
-  def self.released
-    where("released_on <= ?", Time.now).order("released_on desc")
-  end
 
-  def self.hits
-    where('total_gross >= 300000000').order(total_gross: :desc)
-  end
-
-  def self.flops
-    where('total_gross < 50000000').order(total_gross: :asc)
-  end
-
+  scope :released, -> {where("released_on <= ?", Time.now).order(released_on: :desc)}
+  scope :upcoming, -> {where("released_on > ?", Time.now).order(released_on: :asc)}
+  scope :hits, -> {released.where('total_gross >= 300000000').order(total_gross: :desc)}
+  scope :flops, -> {released.where('total_gross < 50000000').order(total_gross: :asc)}
+  scope :rated, ->(rating) {released.where(rating: rating)}
+  scope :recent, ->(max=5) {released.limit(max)}
   def self.recently_added
     order('created_at desc').limit(3)
   end
